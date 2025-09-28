e8c924274fa5db4df1ae0211cc288492d7dfe27e
Elias <eliashaider95@gmail.com>
2025-09-28 23:56:25 +0200

    fix expiration check at email confirmation statement

--- src/server/src/app/persist/sql/update_user_email_verified.sql
+++ src/server/src/app/persist/sql/update_user_email_verified.sql
@@ -3,4 +3,4 @@ SET email_verified = TRUE
 WHERE id = $1
 	AND email_confirmation_hash = $2
 	AND email_verified = FALSE
-	AND created_at + INTERVAL '1 day' < now() AT TIME ZONE 'UTC';
+	AND created_at + INTERVAL '1 day' > now();
