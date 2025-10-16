cc913b1bc6e1d32d12a577724c7d0e3340981e1a
Elias <eliashaider95@gmail.com>
2025-10-16 23:07:03 +0200

    explore gleam js/dts output from ts

--- src/mobile_client/src/App.test.tsx
+++ src/mobile_client/src/App.test.tsx
@@ -1,8 +1,62 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import * as Json from "@shared/gleam_json/gleam/json.mjs";
+import * as SharedUser from "@shared/shared/shared_user.mjs";
+import * as Prelude from "@shared/prelude.mjs";
 
 test("renders without crashing", () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
+});
+
+test("decodes data utilizing @shared (gleam built)", () => {
+  const user = new SharedUser.UserDto(
+    new SharedUser.UserId("id"),
+    new SharedUser.Username("username"),
+    "email@email.com",
+    true,
+  );
+
+  const json = SharedUser.to_json(user);
+
+  expect(json).toEqual({
+    id: "id",
+    username: "username",
+    email: "email@email.com",
+    email_verified: true,
+  });
+
+  const result: Prelude.Result<SharedUser.UserDto, Json.DecodeError$> =
+    Json.parse(Json.to_string(json), SharedUser.decode_user_dto());
+
+  if (result instanceof Prelude.Ok) {
+    const actual: SharedUser.UserDto = result[0]; // NOTE: doesn't get infered -.-
+    expect(actual).toEqual(user);
+  } else {
+    throw new Error("aint ok");
+  }
+});
+
+test("withFields updates immutablely (gleam stdlib)", () => {
+  const user = new SharedUser.UserDto(
+    new SharedUser.UserId("user-123"),
+    new SharedUser.Username("alice"),
+    "old@email.com",
+    false,
+  );
+
+  const updatedUser = user.withFields({
+    email: "new@email.com",
+    email_verified: true,
+  });
+
+  expect(user.email).toBe("old@email.com");
+  expect(user.email_verified).toBe(false);
+
+  expect(updatedUser.email).toBe("new@email.com");
+  expect(updatedUser.email_verified).toBe(true);
+
+  expect(updatedUser.id.v).toBe("user-123");
+  expect(updatedUser.username.v).toBe("alice");
 });
--- src/mobile_client/vite.config.ts
+++ src/mobile_client/vite.config.ts
@@ -1,18 +1,21 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy()],
+  resolve: {
+    alias: {
+      "@shared": path.resolve(__dirname, "../shared/build/dev/javascript"),
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
