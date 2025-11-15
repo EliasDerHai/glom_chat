a4640851347d131f8dfe288830eff377ca841083
Elias <eliashaider95@gmail.com>
2025-11-15 22:53:14 +0100

    fix setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -18,12 +18,21 @@ echo "Checking for required dependencies..."
 check_command "gleam"
 check_command "erl"
 check_command "rebar3"
+check_command "pnpm"
 echo "All dependencies are present."
 
 echo "
 Configuring Git hooks..."
 git config core.hooksPath ./scripts/githooks
 echo "Git hooks path configured."
+
+echo "
+Loading node-slob..."
+(cd src/mobile_client && pnpm i)
+
+echo "
+Building shared js..." # I've added some tests to explore interaction with js output - should probably be removed but I'm a bit of a hoarder
+(cd src/shared && gleam build -t javascript)
 
 echo "
 Running tests to verify setup..."
